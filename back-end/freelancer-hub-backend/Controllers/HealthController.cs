9fbcdf0fffcfa7beae5d5b857109f8cce5d333f9
Fhuller <fhullerbr@gmail.com>
2025-09-02 19:36:23 -0300

    add healthcheck endpoint

---
+++ back-end/freelancer-hub-backend/Controllers/HealthController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace freelancer_hub_backend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult Get()
+        {
+            return Ok("OK");
+        }
+    }
+}
